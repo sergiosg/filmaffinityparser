f0fe2e3f46acc2ed5a398a563e381509600332d9
Sergio Sánchez González <sergiosg@gmail.com>
2020-05-17 16:14:12 +0200

    :lipstick: Clean code

--- src/main/java/sergiosg/SpringBootApp.java
+++ src/main/java/sergiosg/SpringBootApp.java
@@ -6,7 +6,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.util.ObjectUtils;
-import org.springframework.util.StringUtils;
 
 import java.nio.file.NoSuchFileException;
 
